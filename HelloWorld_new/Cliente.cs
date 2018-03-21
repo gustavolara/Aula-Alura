1b52cf36edd4e63f6539607c4ac7188d8409f264
Gustavo Lara Vaz <gulara19@gmail.com>
2018-03-21 20:18:22 -0300

    Arquivos das Aulas

---
+++ HelloWorld_new/Cliente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld_new
+{
+    class Cliente
+    {
+        public string Nome { get; set; }
+        public string Rg { get; set; }
+        public string Endereco { get; set; }
+        public string Cpf { get; set; }
+        public int Idade { get; set; }
+        public bool maiorDeIdade()
+        {
+            return (this.Idade >= 18);
+        }
+        public Cliente(string nome)
+        {
+            this.Nome = nome;
+        }
+    }
+}
---
+++ HelloWorld_new/conta.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld_new
+{
+    class Conta
+    {
+        public int numero { get; set; }
+        public double Saldo { get; private set; }
+        public int agencia;
+        public Cliente titular { get; set; }
+        public bool Saca(double valorASacar)
+        {
+            if (valorASacar < 0 || this.Saldo < valorASacar)
+            {
+                return false;
+            }
+            else
+            {
+                if (this.titular.maiorDeIdade())
+                {
+                    this.Saldo -= valorASacar;
+                    return true;
+                }
+                else
+                {
+                    if(valorASacar <= 200.0)
+                    {
+                        this.Saldo -= valorASacar;
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+        public void Deposita(double valorADepositar)
+        {
+            if (valorADepositar > 0)
+            {
+                this.Saldo += valorADepositar;
+            }
+        }
+        public void Transfere(double valor, Conta destino)
+        {
+            this.Saca(valor);
+            destino.Deposita(valor);
+        }
+        public double CalculaRendimentoAnual()
+        {
+            double saldoNaqueleMes = this.Saldo;
+            for (int i = 0; i < 12; i++)
+            {
+                saldoNaqueleMes = saldoNaqueleMes * 1.007;
+            }
+            double rendimento = saldoNaqueleMes - this.Saldo;
+            return rendimento;
+        }
+    }
+}
